7bffd676a638ba2afcbee3c1e6747b9b67d283b9
Henry Twist <henrytwistprojects@gmail.com>
2023-01-04 22:47:05 +0000

    Day 21 and 22 (Problem 1)

---
+++ Advent of Code 2022/Solutions/Solution21.cs
@@ -0,0 +1,335 @@
+﻿using System.Text.RegularExpressions;
+using AdventOfCode.Framework;
+
+namespace AdventOfCode2022.Solutions;
+
+/// <summary>
+/// https://adventofcode.com/2022/day/21
+/// </summary>
+[Solution(21)]
+[SolutionInput("Input21.test.txt")]
+[SolutionInput("Input21.txt", Benchmark = true)]
+public class Solution21 : Solution
+{
+    private static readonly Regex IndependentMonkeyPattern = new(@"^(.+): (\d+)$");
+    private static readonly Regex DependentMonkeyPattern = new(@"^(.+): (.+) ([\+-\/\*]) (.+)$");
+
+    private readonly List<IndependentMonkey> independentMonkeys;
+
+    private readonly List<OperatorDependentMonkey> dependentMonkeys;
+
+    private readonly OperatorDependentMonkey root;
+
+    public Solution21(Input input) : base(input)
+    {
+        independentMonkeys = new List<IndependentMonkey>(Input.Lines.Length);
+        dependentMonkeys = new List<OperatorDependentMonkey>(Input.Lines.Length);
+
+        foreach (string line in Input.Lines)
+        {
+            Match independentMatch = IndependentMonkeyPattern.Match(line);
+            Match dependentMatch = DependentMonkeyPattern.Match(line);
+
+            if (independentMatch.Success)
+            {
+                IndependentMonkey monkey = new(
+                    independentMatch.Groups[1].Value,
+                    long.Parse(independentMatch.Groups[2].Value));
+                independentMonkeys.Add(monkey);
+            }
+            else if (dependentMatch.Success)
+            {
+                OperatorDependentMonkey monkey = new(
+                    dependentMatch.Groups[1].Value,
+                    GetOperator(dependentMatch.Groups[3].Value),
+                    dependentMatch.Groups[2].Value,
+                    dependentMatch.Groups[4].Value);
+                dependentMonkeys.Add(monkey);
+
+                if (monkey.Name == "root")
+                {
+                    root = monkey;
+                }
+            }
+            else
+            {
+                throw new Exception($"Could not parse monkey {line}");
+            }
+        }
+
+        if (root is null)
+        {
+            throw new Exception("Could not find root monkey");
+        }
+    }
+
+    private static BijectiveOperator GetOperator(string str)
+    {
+        return str switch
+        {
+            "+" => BijectiveOperators.Addition,
+            "-" => BijectiveOperators.Subtraction,
+            "*" => BijectiveOperators.Multiplication,
+            "/" => BijectiveOperators.Division,
+            _ => throw new ArgumentOutOfRangeException(nameof(str))
+        };
+    }
+
+    /// <inheritdoc />
+    protected override string? Problem1()
+    {
+        Dictionary<string, long> yelledNumbers = independentMonkeys.ToDictionary(monkey => monkey.Name, monkey => monkey.Number);
+        Queue<DependentMonkey> pendingMonkeys = new(dependentMonkeys);
+
+        while (pendingMonkeys.Count > 0)
+        {
+            DependentMonkey monkey = pendingMonkeys.Dequeue();
+
+            if (!monkey.TryUpdateYelledNumbers(yelledNumbers))
+            {
+                pendingMonkeys.Enqueue(monkey);
+            }
+        }
+
+        return yelledNumbers[root.Name].ToString();
+    }
+
+    /// <inheritdoc />
+    protected override string? Problem2()
+    {
+        const string MyName = "humn";
+        
+        Dictionary<string, long> yelledNumbers = independentMonkeys
+            .Where(monkey => monkey.Name != MyName)
+            .ToDictionary(monkey => monkey.Name, monkey => monkey.Number);
+        
+        Queue<DependentMonkey> pendingMonkeys = new(dependentMonkeys.Where(monkey => monkey != root));
+        pendingMonkeys.Enqueue(new EqualityDependentMonkey(root.Name, root.Operand1Name, root.Operand2Name));
+        
+        while (pendingMonkeys.Count > 0)
+        {
+            DependentMonkey monkey = pendingMonkeys.Dequeue();
+
+            if (!monkey.TryUpdateYelledNumbers(yelledNumbers))
+            {
+                pendingMonkeys.Enqueue(monkey);
+            }
+        }
+
+        return yelledNumbers[MyName].ToString();
+    }
+
+    private abstract class Monkey
+    {
+        public readonly string Name;
+
+        protected Monkey(string name)
+        {
+            Name = name;
+        }
+    }
+
+    private class IndependentMonkey : Monkey
+    {
+        public readonly long Number;
+
+        public IndependentMonkey(string name, long number) : base(name)
+        {
+            Number = number;
+        }
+
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return $"{Name}: {Number}";
+        }
+    }
+    
+    private abstract class DependentMonkey : Monkey
+    {
+        /// <inheritdoc />
+        protected DependentMonkey(string name) : base(name)
+        {
+        }
+
+        public abstract bool TryUpdateYelledNumbers(Dictionary<string, long> yelledNumbers);
+    }
+
+    private class OperatorDependentMonkey : DependentMonkey
+    {
+        private readonly BijectiveOperator op;
+
+        public readonly string Operand1Name;
+
+        public readonly string Operand2Name;
+
+        public OperatorDependentMonkey(string name, BijectiveOperator op, string operand1Name, string operand2Name) : base(name)
+        {
+            this.op = op;
+            Operand1Name = operand1Name;
+            Operand2Name = operand2Name;
+        }
+
+        /// <inheritdoc />
+        public override bool TryUpdateYelledNumbers(Dictionary<string, long> yelledNumbers)
+        {
+            bool foundOperand1 = yelledNumbers.TryGetValue(Operand1Name, out long operand1);
+            bool foundOperand2 = yelledNumbers.TryGetValue(Operand2Name, out long operand2);
+
+            if (foundOperand1 && foundOperand2)
+            {
+                yelledNumbers.Add(Name, op.Invoke(operand1, operand2));
+                return true;
+            }
+            else
+            {
+                bool foundResult = yelledNumbers.TryGetValue(Name, out long result);
+
+                if (foundResult && foundOperand1)
+                {
+                    yelledNumbers.Add(Operand2Name, op.SolveForOperand2(operand1, result));
+                    return true;
+                }
+                else if (foundResult && foundOperand2)
+                {
+                    yelledNumbers.Add(Operand1Name, op.SolveForOperand1(operand2, result));
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    private class EqualityDependentMonkey : DependentMonkey
+    {
+        private readonly string operand1Name;
+
+        private readonly string operand2Name;
+
+        /// <inheritdoc />
+        public EqualityDependentMonkey(string name, string operand1Name, string operand2Name) : base(name)
+        {
+            this.operand1Name = operand1Name;
+            this.operand2Name = operand2Name;
+        }
+
+        /// <inheritdoc />
+        public override bool TryUpdateYelledNumbers(Dictionary<string, long> yelledNumbers)
+        {
+            if (yelledNumbers.TryGetValue(operand1Name, out long operand1))
+            {
+                yelledNumbers.Add(operand2Name, operand1);
+                return true;
+            }
+            else if (yelledNumbers.TryGetValue(operand2Name, out long operand2))
+            {
+                yelledNumbers.Add(operand1Name, operand2);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    private abstract class BijectiveOperator
+    {
+        public abstract long Invoke(long operand1, long operand2);
+
+        public abstract long SolveForOperand1(long operand2, long result);
+
+        public abstract long SolveForOperand2(long operand1, long result);
+    }
+
+    private abstract class SimpleBijectiveOperator : BijectiveOperator
+    {
+        /// <inheritdoc />
+        public override long SolveForOperand2(long operand1, long result)
+        {
+            return SolveForOperand1(operand1, result);
+        }
+    }
+
+    private class AdditionOperator : SimpleBijectiveOperator
+    {
+        /// <inheritdoc />
+        public override long Invoke(long operand1, long operand2)
+        {
+            return operand1 + operand2;
+        }
+
+        /// <inheritdoc />
+        public override long SolveForOperand1(long operand2, long result)
+        {
+            return result - operand2;
+        }
+    }
+
+    private class SubtractionOperator : BijectiveOperator
+    {
+        /// <inheritdoc />
+        public override long Invoke(long operand1, long operand2)
+        {
+            return operand1 - operand2;
+        }
+
+        /// <inheritdoc />
+        public override long SolveForOperand1(long operand2, long result)
+        {
+            return result + operand2;
+        }
+
+        /// <inheritdoc />
+        public override long SolveForOperand2(long operand1, long result)
+        {
+            return operand1 - result;
+        }
+    }
+
+    private class MultiplicationOperator : SimpleBijectiveOperator
+    {
+        /// <inheritdoc />
+        public override long Invoke(long operand1, long operand2)
+        {
+            return operand1 * operand2;
+        }
+
+        /// <inheritdoc />
+        public override long SolveForOperand1(long operand2, long result)
+        {
+            return result / operand2;
+        }
+    }
+
+    private class DivisionOperator : BijectiveOperator
+    {
+        /// <inheritdoc />
+        public override long Invoke(long operand1, long operand2)
+        {
+            return operand1 / operand2;
+        }
+
+        /// <inheritdoc />
+        public override long SolveForOperand1(long operand2, long result)
+        {
+            return result * operand2;
+        }
+
+        /// <inheritdoc />
+        public override long SolveForOperand2(long operand1, long result)
+        {
+            return operand1 / result;
+        }
+    }
+
+    private static class BijectiveOperators
+    {
+        public static readonly BijectiveOperator Addition = new AdditionOperator();
+        public static readonly BijectiveOperator Subtraction = new SubtractionOperator();
+        public static readonly BijectiveOperator Multiplication = new MultiplicationOperator();
+        public static readonly BijectiveOperator Division = new DivisionOperator();
+    }
+}
---
+++ Advent of Code 2022/Solutions/Solution22.cs
@@ -0,0 +1,321 @@
+﻿using AdventOfCode.Framework;
+
+namespace AdventOfCode2022.Solutions;
+
+/// <summary>
+/// https://adventofcode.com/2022/day/22
+/// </summary>
+[Solution(22)]
+[SolutionInput("Input22.test.txt")]
+[SolutionInput("Input22.txt", Benchmark = true)]
+public class Solution22 : Solution
+{
+    private readonly List<MapRow> mapRows;
+
+    private readonly List<IInstruction> instructions;
+
+    public Solution22(Input input) : base(input)
+    {
+        mapRows = new List<MapRow>(Input.Lines.Length);
+
+        foreach (string line in Input.Lines)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                break;
+            }
+
+            int rowStartIndex = line.IndexOfAny(new[] { '.', '#' });
+
+            if (rowStartIndex == -1)
+            {
+                throw new Exception($"Could not parse row: {line}");
+            }
+
+            List<Tile> rowTiles = line.Skip(rowStartIndex).Select(GetTile).ToList();
+            mapRows.Add(new MapRow(rowStartIndex, rowTiles));
+        }
+
+        string path = Input.Lines[^1];
+        instructions = new List<IInstruction>();
+
+        int countBuffer = 0;
+        foreach (char c in path)
+        {
+            if (char.IsDigit(c))
+            {
+                countBuffer *= 10;
+                countBuffer += c - '0';
+            }
+            else
+            {
+                instructions.Add(new MoveInstruction(countBuffer));
+                countBuffer = 0;
+
+                instructions.Add(new TurnInstruction(GetTurn(c)));
+            }
+        }
+        instructions.Add(new MoveInstruction(countBuffer));
+    }
+
+    private static Tile GetTile(char c)
+    {
+        return c switch
+        {
+            '.' => Tile.Open,
+            '#' => Tile.Wall,
+            _ => throw new ArgumentOutOfRangeException(nameof(c))
+        };
+    }
+
+    private static TurnInstruction.TurnDirection GetTurn(char c)
+    {
+        return c switch
+        {
+            'R' => TurnInstruction.TurnDirection.Clockwise,
+            'L' => TurnInstruction.TurnDirection.Counterclockwise,
+            _ => throw new ArgumentOutOfRangeException(nameof(c))
+        };
+    }
+
+    /// <inheritdoc />
+    protected override string? Problem1()
+    {
+        Position initialPosition = new(mapRows[0].GetStartX(), 0, Position.Direction.Right);
+        Map map = new(mapRows, initialPosition);
+
+        foreach (IInstruction instruction in instructions)
+        {
+            instruction.Execute(map);
+        }
+
+        int row = map.Position.Y + 1;
+        int column = map.Position.X + 1;
+        int facing = map.Position.Facing switch
+        {
+            Position.Direction.Left => 2,
+            Position.Direction.Up => 3,
+            Position.Direction.Right => 0,
+            Position.Direction.Down => 1,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+
+        int password = (1_000 * row) + (4 * column) + facing;
+        return password.ToString();
+    }
+
+    /// <inheritdoc />
+    protected override string? Problem2()
+    {
+        return null;
+    }
+
+    private record MapRow(int X, IReadOnlyList<Tile> Tiles)
+    {
+        public int GetStartX()
+        {
+            for (int i = 0; i < Tiles.Count; i++)
+            {
+                if (Tiles[i] == Tile.Open)
+                {
+                    return X + i;
+                }
+            }
+
+            throw new Exception("Could not find a start position for the row");
+        }
+
+        public bool HasTileAt(int x)
+        {
+            return 0 <= x - X && x - X < Tiles.Count;
+        } 
+
+        public Tile GetTile(int x)
+        {
+            return Tiles[x - X];
+        }
+    }
+
+    private enum Tile
+    {
+        Open,
+        Wall
+    }
+
+    private interface IInstruction
+    {
+        void Execute(Map map);
+    }
+
+    private class MoveInstruction : IInstruction
+    {
+        private readonly int count;
+
+        public MoveInstruction(int count)
+        {
+            this.count = count;
+        }
+
+        /// <inheritdoc />
+        public void Execute(Map map)
+        {
+            switch (map.Position.Facing)
+            {
+                case Position.Direction.Left:
+                    MoveHorizontally(map, -1);
+                    break;
+                case Position.Direction.Up:
+                    MoveVertically(map, -1);
+                    break;
+                case Position.Direction.Right:
+                    MoveHorizontally(map, 1);
+                    break;
+                case Position.Direction.Down:
+                    MoveVertically(map, 1);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private void MoveHorizontally(Map map, int step)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                MapRow row = map.MapRows[map.Position.Y];
+
+                int nextX = map.Position.X + step;
+
+                if (nextX == row.X - 1)
+                {
+                    nextX = row.X + row.Tiles.Count - 1;
+                }
+                else if (nextX == row.X + row.Tiles.Count)
+                {
+                    nextX = row.X;
+                }
+
+                Tile tile = row.GetTile(nextX);
+
+                if (tile == Tile.Wall)
+                {
+                    break;
+                }
+
+                map.Position.X = nextX;
+            }
+        }
+
+        private void MoveVertically(Map map, int step)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                int nextY = map.Position.Y + step;
+
+                if (nextY == -1 ||
+                    nextY == map.MapRows.Count ||
+                    !map.MapRows[nextY].HasTileAt(map.Position.X) ||
+                    !map.MapRows[nextY].HasTileAt(map.Position.X))
+                {
+                    nextY = FindLastY(map, -step);
+                }
+
+                MapRow row = map.MapRows[nextY];
+                Tile tile = row.GetTile(map.Position.X);
+
+                if (tile == Tile.Wall)
+                {
+                    break;
+                }
+
+                map.Position.Y = nextY;
+            }
+        }
+
+        private static int FindLastY(Map map, int step)
+        {
+            int y = map.Position.Y + step;
+            while (true)
+            {
+                if (y < 0 | y >= map.MapRows.Count || !map.MapRows[y].HasTileAt(map.Position.X))
+                {
+                    return y - step;
+                }
+                y += step;
+            }
+        }
+    }
+
+    private class TurnInstruction : IInstruction
+    {
+        private readonly TurnDirection turn;
+
+        public TurnInstruction(TurnDirection turn)
+        {
+            this.turn = turn;
+        }
+
+        /// <inheritdoc />
+        public void Execute(Map map)
+        {
+            int rotationDirection = turn switch
+            {
+                TurnDirection.Clockwise => 1,
+                TurnDirection.Counterclockwise => -1,
+                _ => throw new ArgumentOutOfRangeException(nameof(turn))
+            };
+
+            int directionCount = 4;
+            map.Position.Facing = (Position.Direction)((((int)map.Position.Facing + rotationDirection) + directionCount) % directionCount);
+        }
+
+        public enum TurnDirection
+        {
+            Clockwise,
+            Counterclockwise
+        }
+    }
+
+    private class Position
+    {
+        public int X;
+
+        public int Y;
+
+        public Direction Facing;
+
+        public Position(int x, int y, Direction facing)
+        {
+            X = x;
+            Y = y;
+            Facing = facing;
+        }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return $"({X}, {Y}) ~ {Facing}";
+        }
+
+        public enum Direction
+        {
+            Left,
+            Up,
+            Right,
+            Down
+        }
+    }
+
+    private class Map
+    {
+        public readonly IReadOnlyList<MapRow> MapRows;
+
+        public readonly Position Position;
+
+        public Map(IReadOnlyList<MapRow> mapRows, Position position)
+        {
+            MapRows = mapRows;
+            Position = position;
+        }
+    }
+}
